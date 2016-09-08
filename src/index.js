22983afe15a59c891a576c52b3a127fea8b8d161
jmconde <jose.conde@gmail.com>
2016-09-08 07:36:40 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-/**
- * morningstar-base-charts
- *
- * Copyright © 2016 . All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
 "use strict";
 
 import Factory from "./js/app.js";
